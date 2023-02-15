19daf5a30c05a7e2160def7ef4e7196df9ae2284
jiaqili0714 <ali38@smith.edu>
2023-02-15 17:09:56 -0500

    A3 finished

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,65 @@
-class Conversation {
+import java.util.Scanner;
 
+class Conversation {
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    //construct mirrored words and canned reponses
+    String[] PremadeResponses={"Mmm-hm.","Interesting!","Oh really?","I'm glad to hear that! Can u tell me more about it?","Wow"};
+    Scanner userInput = new Scanner(System.in);
+    
+    //ask for user input
+    System.out.println("How many rounds do you wanna play?:");
+    int rounds = userInput.nextInt();
+    userInput.nextLine();
+
+
+    //create empty array for transcript
+    String[] transcript=new String[rounds*2+1];
+
+    //start conversation and store first sentence into transcript
+    transcript[0]="Hi there! What's on your mind?";
+    System.out.println(transcript[0]);
+
+    /**
+     * replace words in user input
+     */
+    for (int i=0;i<rounds;i++){
+      String user = userInput.nextLine();
+      String mirrored;
+      String changed_words = user.replace("I ","you ");
+      changed_words = changed_words.replace("i ","you");
+      changed_words = changed_words.replace(" me","you");
+      changed_words = changed_words.replace("Me ","you");
+      changed_words = changed_words.replace(" am ","are");
+      changed_words = changed_words.replace("Am ","are");
+      changed_words = changed_words.replace(" my ","your");
+      changed_words = changed_words.replace("My ","your");
+      changed_words = changed_words.replace(" your ","my");
+      changed_words = changed_words.replace("Your ","my");
+      changed_words = changed_words.replace(" you"," I");
+      changed_words = changed_words.replace("You ","I");
+      changed_words = changed_words.replace("I am ","You're ");
+      changed_words = changed_words.replace("I'm ","You're ");
+
+      //if changed words are different from original user input, then changed=mirrored, else choose random canned phrase
+      if(!changed_words.equals(user)){
+        mirrored =changed_words;
+      } else{
+      //random number generator idea comes from https://stackoverflow.com/a/363732
+        mirrored=PremadeResponses[(int)(Math.random() * ((4 - 0) + 1))];
+      }
+      System.out.println(mirrored);
+      
+      //store coversations into transcript
+      //index of odd number are always user input, and index of even number are mirrored response.
+      transcript[2*i+1]=user;
+      transcript[2*i+2]=mirrored;
+    }
+  System.out.println("Bye see you next time!");
+  System.out.println(" ");
+  System.out.println("Transcript:");
+
+for(int i=0;i<transcript.length;i++){
+    System.out.println(transcript[i]);
+  }
   }
 }
--- reflection.md
+++ reflection.md
@@ -5,3 +5,11 @@ Use this file to record your reflection on this assignment.
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+I asked Camila about why should we define rounds and how it relates to the for loop. I attended the Tueseday TA and asked Chris about why my transcript didn't show up and it turned out that I put the for loop in the wrong bracket.
+
+My initial impression of Java is that it's so hard to use, compared to Python. For instance, asking for user input would be so easy in Python with only one line of code, but in Java I need to create a new scanner object and refer back to it whenever I want to receive another user input.
+
+Though Java seems a bit rigid in terms of syntax and format, I can tell that it is a more structured way of programming, which makes it easy to create reusable code.
+
+For this assignment, I struggled mainly with Java syntax and also indentation. Especially when writting for loops, without indentation, the brackets made it very complicated to tell whether or not is the code inside the loop. Plus that VS code kept telling me that there are some problems in my code when I haven't finish the whole thing, I was really confused about what's the correct syntax. It's good to get yourself used to java syntax and also write pseudo code before starting the assignment, because I feel like it's relatively more difficult to test out things in Java than Python due to all the format restrictions.
